e480dbec078c452bbd01d8578d48e4d3e86bbc36
Kai L <kailin8591@gmail.com>
2022-06-15 20:56:05 -0400

    pagination

--- js/index.js
+++ js/index.js
@@ -1,14 +1,42 @@
+let pageNumber = 0;
+const numberItemPerPage = 50;
+let allMonstersArray;
+const pageNumberAndDisplay = (monstersArray) => {
+  const startIndex = pageNumber * numberItemPerPage;
+  const endIndex = startIndex + numberItemPerPage;
+  document.querySelector('#monster-container').querySelectorAll('*').forEach((n) => n.remove());
+  for (let i = startIndex; i < monstersArray.length && i < endIndex; i++) {
+    const monstersContainer = document.querySelector('#monster-container');
+    const monsterInfoTag = document.createElement('div');
+    const element = monstersArray[i];
+    const monstersList = `Name: ${element.name} Age: ${element.age} Description: ${element.description}\n`;
+    monsterInfoTag.textContent = monstersList;
+    monsterInfoTag.appendChild(document.createElement('br'));
+    monsterInfoTag.appendChild(document.createElement('br'));
+    monstersContainer.appendChild(monsterInfoTag);
+  }
+};
+
+const backButton = document.querySelector('#back');
+const forwardButton = document.querySelector('#forward');
+backButton.addEventListener('click', (event) => {
+  if (pageNumber > 0) {
+    pageNumber -= 1;
+    pageNumberAndDisplay(allMonstersArray);
+  }
+});
+forwardButton.addEventListener('click', (event) => {
+  if (pageNumber * numberItemPerPage < allMonstersArray.length) {
+    pageNumber += 1;
+    pageNumberAndDisplay(allMonstersArray);
+  }
+});
+
 fetch('http://localhost:3000/monsters')
   .then((res) => res.json())
   .then((data) => {
-    for (let i = 0; i < data.length && i <= 50; i++) {
-      const monstersContainer = document.querySelector('#monster-container');
-      const monsterInfoTag = document.createElement('span');
-      const element = data[i];
-      const monstersList = `Name: ${element.name} Age: ${element.age} Description: ${element.description}`;
-      monsterInfoTag.textContent = monstersList;
-      monstersContainer.appendChild(monsterInfoTag);
-    }
+    pageNumberAndDisplay(data);
+    allMonstersArray = data;
 
     const formTag = document.querySelector('#create-monster');
 
@@ -44,25 +72,21 @@ fetch('http://localhost:3000/monsters')
 
     form.addEventListener('submit', (event) => {
       event.preventDefault();
+      const newMonster = {
+        name: event.target.monster_name.value,
+        age: parseFloat(event.target.monster_age.value),
+        description: event.target.monster_description.value,
+      };
+      allMonstersArray.push(newMonster);
+      pageNumberAndDisplay(allMonstersArray);
 
-      fetch("http://localhost:3000/monsters", {
+      fetch('http://localhost:3000/monsters', {
         method: 'POST',
         headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
         },
-        body: json.stringify () {
-   
-   "Customer": "Jason Sweet",
-   "Quantity": 1,
-   "Price": 18.00
-  },
+        body: JSON.stringify(newMonster),
       });
-
-      response.json().then(data => {
-        console.log(data);
-      });
-
-
     });
   });
